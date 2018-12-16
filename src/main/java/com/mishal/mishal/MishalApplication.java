642b306d9508a552fd7a7d31183f834411ac2739
guri <guri.kamelo@gmail.com>
2018-12-16 22:05:04 +0200

    first

---
+++ src/main/java/com/mishal/mishal/MishalApplication.java
@@ -0,0 +1,17 @@
+package com.mishal.mishal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishalApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Mishal");
+		SpringApplication.run(MishalApplication.class, args);
+		System.out.println("Mishal2");
+
+	}
+
+}
+
